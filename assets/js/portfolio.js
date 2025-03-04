836dff53f0be6bb49b7a3b0d53dedcc25d2e3e04
Michael Liang <michaelly0128@gmail.com>
2025-03-04 10:06:12 -0600

    Add portfolio functionality with Swiper integration and update i18n files

---
+++ assets/js/portfolio.js
@@ -0,0 +1,24 @@
+// portfolio.js
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./portfolio.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("portfolio-section").innerHTML = data;
+      initPortfolioSwiper();
+    });
+});
+
+function initPortfolioSwiper() {
+  let swiperPortfolio = new Swiper(".portfolio_more_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
